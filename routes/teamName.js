1fd7ab2dbceb7d64daff5043d40b7ef3a8b44d4c
kBarbz <kbarbz1995@gmail.com>
2019-08-27 10:17:26 +0800

    added full root for json

--- routes/teamName.js
+++ routes/teamName.js
@@ -18,7 +18,7 @@ const fs = require('fs');
 }
 
 let bedford;
-jsonReader('/public/images/bedfordJSON.json', (err, team) => {
+jsonReader('D:/home/site/wwwroot/public/images>bedfordJSON.json', (err, team) => {
     if (err) {
         console.log(err)
         return
@@ -27,7 +27,7 @@ jsonReader('/public/images/bedfordJSON.json', (err, team) => {
 })
 
 let bm1;
-jsonReader('/public/images/bayswater1JSON.json', (err, team) => {
+jsonReader('D:/home/site/wwwroot/public/images/bayswater1JSON.json', (err, team) => {
     if (err) {
         console.log(err)
         return
@@ -36,7 +36,7 @@ jsonReader('/public/images/bayswater1JSON.json', (err, team) => {
 })
 
 let bm2;
-jsonReader('/public/images/bayswater2JSON.json', (err, team) => {
+jsonReader('D:/home/site/wwwroot/public/images/bayswater2JSON.json', (err, team) => {
     if (err) {
         console.log(err)
         return
@@ -45,7 +45,7 @@ jsonReader('/public/images/bayswater2JSON.json', (err, team) => {
 })
 
 let bm3;
-jsonReader('./public/images/bayswater3JSON.json', (err, team) => {
+jsonReader('D:/home/site/wwwroot/public/images/bayswater3JSON.json', (err, team) => {
     if (err) {
         console.log(err)
         return
@@ -54,7 +54,7 @@ jsonReader('./public/images/bayswater3JSON.json', (err, team) => {
 })
 
 let f1;
-jsonReader('/public/images/fremantle1JSON.json', (err, team) => {
+jsonReader('D:/home/site/wwwroot/public/images/fremantle1JSON.json', (err, team) => {
     if (err) {
         console.log(err)
         return
@@ -63,7 +63,7 @@ jsonReader('/public/images/fremantle1JSON.json', (err, team) => {
 })
 
 let f2;
-jsonReader('/public/images/fremantle2JSON.json', (err, team) => {
+jsonReader('D:/home/site/wwwroot/public/images/fremantle2JSON.json', (err, team) => {
     if (err) {
         console.log(err)
         return
